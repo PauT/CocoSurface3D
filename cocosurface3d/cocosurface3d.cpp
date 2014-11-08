1218b6d6bc0b04d7ffa10cab68bab1c3c1647c43
PauT <pauttony@gmail.com>
2014-11-08 17:34:58 +0800

    +commit

--- cocosurface3d/cocosurface3d.cpp
+++ cocosurface3d/cocosurface3d.cpp
@@ -20,11 +20,5 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
 
-#ifndef __COCOSURFACE3D__H__
-#define __COCOSURFACE3D__H__
-// 0x00 HI ME LO
-// 00   00 00 01
-#define COCOSURFACE3D_VERSION 0x00000001
-#include "cocos2d.h"
 
-#endif
+
